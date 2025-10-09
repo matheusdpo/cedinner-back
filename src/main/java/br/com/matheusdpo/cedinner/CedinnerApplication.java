1e2957a553c450f7ac0f55f9367abd5ccdcac1c2
matheus <matheusoliveira1991@hotmail.com>
2025-10-09 20:08:25 -0300

    first commit

---
+++ src/main/java/br/com/matheusdpo/cedinner/CedinnerApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusdpo.cedinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedinnerApplication.class, args);
+	}
+
+}
